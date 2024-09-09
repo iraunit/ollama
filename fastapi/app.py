cd1328a410b826628423159ad49542835ca3d88a
Raunit Verma <ramgopal10258@gmail.com>
2024-09-10 00:57:14 +0530

    first commit

--- fastapi/app.py
+++ fastapi/app.py
@@ -4,16 +4,18 @@ from fastapi import FastAPI, Response
 
 app = FastAPI()
 
+
 @app.get('/')
 def home():
-    return {"hello" : "World"}
+    return {"hello": "World"}
+
 
 @app.get('/ask')
-def ask(prompt :str):
+def ask(prompt: str):
     res = requests.post('http://ollama:11434/api/generate', json={
         "prompt": prompt,
-        "stream" : False,
-        "model" : "llama3"
+        "stream": False,
+        "model": "llama3.1"
     })
 
     return Response(content=res.text, media_type="application/json")
